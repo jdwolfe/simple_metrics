9bb9731e81b75c3c9ea3aa931f97e19cb5c4b135
Jeff Wolfe <jeff@wolfeplanet.net>
2014-12-12 16:35:05 -0600

    added a display title for graph pages minor fixes for data

--- html/index.php
+++ html/index.php
@@ -1,9 +1,14 @@
+<?php
+require_once('../config.php');
+?>
+<!DOCTYPE html>
+
 <html>
 <head>
-<title>Ad Server Metrics</title>
+	<title><?php echo $display_name; ?> Metrics</title>
 </head>
 <body>
-<h1>Ad Server Metrics</h1>
+<h1><?php echo $display_name; ?> Metrics</h1>
 <iframe src="/metrics/db_connections.php" height="600" width="1500"></iframe>
 <iframe src="/metrics/http_connections.php" height="600" width="1500"></iframe>
 <iframe src="/metrics/cpu_load.php" height="600" width="1500"></iframe>
--- html/mini_index.php
+++ html/mini_index.php
@@ -1,14 +1,17 @@
+<?php
+require_once('../config.php');
+?>
 <!DOCTYPE html>
 
 <html>
 
 <head>
-  <title>Server Metrics - Last 2 hours</title>
+  <title><?php echo $display_name; ?> - Last 2 hours</title>
 </head>
 
 <body>
 
-<h1>Server Metrics - Last 2 hours</h1>
+<h1><?php echo $display_name; ?> - Last 2 hours</h1>
 <iframe src="/metrics/db_connections.php?mini=1" frameborder="0" width="350" height="310"></iframe>
 <iframe src="/metrics/http_connections.php?mini=1" frameborder="0" width="350" height="310"></iframe>
 <iframe src="/metrics/cpu_load.php?mini=1" frameborder="0" width="350" height="310"></iframe>
